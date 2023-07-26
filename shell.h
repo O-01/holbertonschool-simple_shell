dad08919faec4e5bc8bd4249943458bb1fffe152
ericpo1sh <ericpo1sh@gmail.com>
2023-07-26 09:57:37 -0700

    Added a header file including necessary libraries

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef __SHELL_H__
+#define __SHELL_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+
+#endif
