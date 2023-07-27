2ae6ede5e1d206ff9dc3e09d840cebbf3a2bb75c
ericpo1sh <ericpo1sh@gmail.com>
2023-07-26 20:48:10 -0700

    Echo command added

---
+++ echo_cmd.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ * echo_cmd - when echo is used, prints everything after it to stdout
+ * @argc: not used here
+ * @argv: arguements in the commandline
+ */
+void echo_cmd(int argc, char *argv[])
+{
+	int i = 1;
+
+	(void)argc;
+
+	if (argv[0] == "echo");
+	{
+		for (; argv[i]; i++)
+			printf("%s", argv[i]);
+		printf("\n");
+	}
+}
