e2a28fff1a70e79b9f2fb81c0556b808f65a4733
O-01 <sa.an-s@yandex.com>
2023-08-05 05:59:28 -0500

    WIP builtIn_exit status arguments

--- builtIn_exit.c
+++ builtIn_exit.c
@@ -10,16 +10,38 @@
 
 int builtIn_exit(char *code, char *input, char **cmdS)
 {
-	int status = 0, cod3 = 0;
+	int status = 0, cod3 = 0, yes = 0, iter = 0;
 
 	if (code)
-		cod3 = atoi(code), status = cod3;
-
-	freecmdS(cmdS);
+	{
+		for (iter = 0; code[iter]; iter++)
+			if (code[iter] < '0' || code[iter] > '9')
+			{
+				yes = 1;
+				break;
+			}
+	}
+	if (yes == 1)
+	{
+		printf("./hsh: 1: %s: Illegal number: %s\n", cmdS[0], code);
+		freecmdS(cmdS);
+		if (input)
+			free(input);
+		exit(2);
+	}
+	cod3 = atoi(code), status = cod3;
+	if (status < 0)
+	{
+		printf("./hsh: 1: %s: Illegal number: %d", cmdS[0], status);
+		freecmdS(cmdS);
+		if (input)
+			free(input);
+		exit(2);
+	}
+/* ^ Above redundant ? */
 	if (input)
 		free(input);
-
-	exit(status);
+	freecmdS(cmdS), exit(status);
 
 	return (0);
 }
